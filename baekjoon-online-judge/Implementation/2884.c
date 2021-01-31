85766f8bb74293b72452f2017eb72dc6d939cdd8
Taehyun Kim <k1m743hyun@gmail.com>
2021-01-31 11:10:54 +0000

    Create 2884.c

---
+++ baekjoon-online-judge/Implementation/2884.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void) {
+  int H, M;
+  scanf("%d %d", &H, &M);
+
+  if (M < 45) {
+    if (H == 0) {
+      printf("%d %d\n", 23, M + 15);
+    }
+
+    else {
+      printf("%d %d\n", H - 1, M + 15);
+    }
+  }
+
+  else {
+    printf("%d %d\n", H, M - 45);
+  }
+}
