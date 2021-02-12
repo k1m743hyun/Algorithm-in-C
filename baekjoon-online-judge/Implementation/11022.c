44508a4a868cb796599e7973d52fc740a99b7316
Taehyun Kim <k1m743hyun@gmail.com>
2021-02-12 05:26:09 +0000

    Create 11022.c

---
+++ baekjoon-online-judge/Implementation/11022.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void) {
+  int n;
+  scanf("%d", &n);
+
+  int a, b;
+  for (int i = 1; i < n + 1; i++) {
+    scanf("%d %d", &a, &b);
+    printf("Case #%d: %d + %d = %d\n", i, a, b, a + b);
+  }
+}
