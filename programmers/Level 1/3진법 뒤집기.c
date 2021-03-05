27e15500440aa389f35e0e8cda9c19e79912ac24
Taehyun Kim <k1m743hyun@gmail.com>
2021-03-05 08:36:22 +0000

    Create 3진법 뒤집기.c

---
+++ programmers/Level 1/3진법 뒤집기.c
@@ -0,0 +1,15 @@
+#include <math.h>
+
+int solution(int n) {
+    int len = (int)(log10(n) / log10(3)) + 1;
+    int numArr[len];
+    for (int i = 0; i < len; i++) {
+        numArr[i] = (int)(n % 3);
+        n /= 3;
+    }
+    int answer = 0;
+    for (int j = len - 1; j > -1; j--) {
+        answer += numArr[j] * pow(3, len - (j + 1));
+    }
+    return answer;
+}
